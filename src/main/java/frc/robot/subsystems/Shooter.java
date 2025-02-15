3018afb5d35ce74f3ecf0ab043fefa4cfcb92a42
Cadyn <ducc11507@student.trentonschools.com>
2025-02-14 19:04:52 -0500

    sensor tell when to slow and stop

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 
 import com.revrobotics.spark.SparkMax;
+import com.reduxrobotics.sensors.canandcolor.Canandcolor;
+import com.reduxrobotics.sensors.canandcolor.CanandcolorSettings;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -12,6 +14,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter extends SubsystemBase{
     SparkMax leadMotor;
     SparkMax followMotor;
+    // Create a new settings object
+    CanandcolorSettings settings;
+    // Creates a Canandcolor object referencing a Canandcolor with CAN ID 3
+    Canandcolor canandcolorSlow;
+    //Creates a Canandcolor object referencing a Canandcolor with CAN ID 0
+    Canandcolor canandcolorStop;  
+
     public Shooter(int leadID, int followID){
         leadMotor = new SparkMax(leadID, MotorType.kBrushless);
         followMotor = new SparkMax(followID, MotorType.kBrushless);
@@ -22,6 +31,16 @@ public class Shooter extends SubsystemBase{
         
         followMotor.configure(followConfig, ResetMode.kResetSafeParameters , PersistMode.kPersistParameters);
 
+        canandcolorSlow = new Canandcolor(20);
+        canandcolorSlow.resetFactoryDefaults();
+        canandcolorStop = new Canandcolor(21);
+        canandcolorStop.resetFactoryDefaults();
+
+        settings = new CanandcolorSettings();
+        settings.setLampLEDBrightness(0);
+
+        canandcolorSlow.setSettings(settings);
+        canandcolorStop.setSettings(settings);
     }
 
     public void shoot(double speed){
@@ -32,5 +51,11 @@ public class Shooter extends SubsystemBase{
     public void stop(){
         leadMotor.set(0);
     }
-
+    
+    public boolean isSlowSensorBlocked() {
+        return canandcolorSlow.getProximity() > .5;
+    }
+    public boolean isStopSensorBlocked() {
+        return canandcolorStop.getProximity() > .5;
+    }
 }
